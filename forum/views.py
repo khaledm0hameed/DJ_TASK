4f9a6bc0a968bb79b5bce1a0ae58e574fad4c479
admin <khmo492007@gmail.com>
2023-07-31 15:51:11 +0300

    add models in admin.py and createsuperuser

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView,DeleteView,CreateView,UpdateView
+from .models import Question,Answers
 # Create your views here.
+
+class Question_list(ListView):
+    model = Question
+    template_name = 'index.html'
+    context_object_name = 'ques'
