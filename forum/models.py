a91db6509eb744c2e7381c2342cd26c3d7f7d385
admin <khmo492007@gmail.com>
2023-07-31 15:45:52 +0300

    create models Question , Answers

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+from taggit.managers import TaggableManager
 # Create your models here.
+
+'''
+Author
+question
+tags
+created_at
+Content
+________________________________
+
+Author
+Answer
+question
+created_at
+'''
+
+
+
+class Question(models.Model):
+    Author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    created_at = models.DateField(default=timezone.now)
+    tags = TaggableManager()
+    question = models.CharField(max_length=20000)
+    Content = models.CharField(max_length=20000)
+
+
+class Answers(models.Model):
+    Author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    created_at = models.DateField(default=timezone.now)
+    Answer = models.CharField(max_length=1000)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name="Answers_Question")
